83f779a1014fb536415e0bb67e9b564210cd81ca
18098423927@163.com <18098423927@163.com>
2021-04-22 18:37:58 +0800

    添加项目文件。

---
+++ MIM_OFDM.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+
+class MIM_OFDM
+{
+public:
+	MIM_OFDM();
+	~MIM_OFDM();
+
+private:
+
+};
+
+MIM_OFDM::MIM_OFDM()
+{
+}
+
+MIM_OFDM::~MIM_OFDM()
+{
+}
---
+++ 无线通信技术实验.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"MIM_OFDM.h"
+using namespace std;
+
+int main(int argc,char **argv)
+{
+	return 0;
+}
